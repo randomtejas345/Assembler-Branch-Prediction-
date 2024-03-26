d773e231570b9619c8522686e52ea9d595e7b238
Priyanshu <120452699+bakasingh@users.noreply.github.com>
2024-03-26 22:16:45 +0530

    Update predictor_3.1.cpp

--- predictor_3.1.cpp
+++ predictor_3.1.cpp
@@ -7,49 +7,58 @@
 
 using namespace std;
 
-int flag = 1;
-int is_branch = 0;
-int is_pos = 0;
-int total = 0;
-int always_taken = 0;
-int always_not_taken = 0;
-int hit_1 = 0;
-int miss_1 = 0;
-int hit_2 = 0;
-int miss_2 = 0;
+int tester = 0;
+long flag = 1;
+long flag_2 = 1;
+long is_branch = 0;
+long is_pos = 0;
+long total = 0;
+long always_taken = 0;
+long always_not_taken = 0;
+long hit_1 = 0;
+long miss_1 = 0;
+long hit_2 = 0;
+long miss_2 = 0;
 
+map<string, pair<long, vector<long>>> BTBonebit;
+map<string, pair<long, vector<long>>> BTBtwobit;
 
-map<string, pair<int, vector<int>>> BTB;
+string last_instruction = "";
 
-void single_bit(string pc, string temp_word, int Taken) {
-    if (BTB[pc].second.back() != Taken) {
+unsigned int hexToDecimal(const string &hexString) {
+    unsigned int decimalValue;
+    stringstream ss;
+    ss << hex << hexString;
+    ss >> decimalValue;
+    return decimalValue;
+}
+
+void single_bit(string pc, string temp_word, long Taken) {
+    if (BTBonebit[pc].second.back() != Taken) {
         miss_1++;
     } else {
         hit_1++;
     }
+    BTBonebit[pc].second.push_back(Taken);
 }
 
-void double_bit(string pc, string temp_word, int Taken) {
-    int size = BTB[pc].second.size();
-    int last = BTB[pc].second[size - 1];
-    if (size > 1) {
-        int second_last = BTB[pc].second[size - 2];
-        if ((last == Taken && second_last == Taken) || (last != Taken && second_last == Taken)) {
-            hit_2++;
-        } else {
-            miss_2++;
-        }
+void double_bit(string pc, string temp_word, long Taken) {
+    long size = BTBtwobit[pc].second.size();
+    long last = BTBtwobit[pc].second[size - 1];
+    long second_last = BTBtwobit[pc].second[size - 2];
+    if ((last == 1 && second_last == 1 && Taken == 1) || (second_last == 0 && last == 0 && Taken == 0)) {
+        hit_2++;
+    } else if ((last == 1 && second_last == 0 && Taken == 0) || (second_last == 1 && last == 0 && Taken == 1)) {
+        hit_2++;
     } else {
-        if (BTB[pc].second.back() != Taken) {
-            hit_2++;
-        } else {
-            miss_2++;
-        }
+        miss_2++;
     }
+    BTBtwobit[pc].second.push_back(Taken);
 }
-int count=0;
+
+long count = 0;
 int main() {
-    printf("1> wikison_test_Lab\n");
+      printf("1> wikison_test_Lab\n");
     printf("2> sqrt_test_Lab\n");
     printf("3> Recursion_test_Lab\n");
     printf("4> recursion_test_Lab\n");
@@ -57,23 +66,17 @@ int main() {
     printf("6> Fact_test_Lab_test_Lab\n");
     printf("7> Bubbel_test_Lab\n");
     printf("8> Sample_test_Lab\n");
-    int num_files = 8; // Number of files to scan
-    vector<string> filenames; // Vector to store filenames
-    for (int i = 1; i <= num_files; ++i) {
-        filenames.push_back("code" + to_string(i) + ".asm"); // Add filenames to vector
-    }
-
     // Prompt user to select a file
-    cout << "Enter the number of the file you want to scan (1-" << num_files << "): ";
+    cout << "Enter the number of the file you want to scan (1-8): ";
     int file_number;
     cin >> file_number;
 
-    if (file_number < 1 || file_number > num_files) {
+    if (file_number < 1 || file_number > 8) {
         cout << "Invalid file number. Exiting..." << endl;
         return 1;
     }
 
-    string filename = filenames[file_number - 1]; // Get selected filename
+    string filename = "code" + to_string(file_number) + ".asm"; // Construct filename
 
     string text;
     ifstream Myfile(filename);
@@ -83,11 +86,16 @@ int main() {
     }
 
     while (getline(Myfile, text)) {
+        string temp = text;
+        text = last_instruction;
         if (!text.empty()) {
             stringstream ss(text);
+            stringstream ss2(temp);
             string temp_word;
+            string temp_word_2;
             string pc;
-            vector<int> history;
+            string pc_present;
+            vector<long> history;
             while (getline(ss, temp_word, ' ')) {
                 if (temp_word.substr(0, 2) == "0x" && flag == 1) {
                     flag = 1 - flag;
@@ -100,13 +108,30 @@ int main() {
                     is_pos = 1;
                 }
             }
-            int label;
-            int Taken;
+            while (getline(ss2, temp_word_2, ' ')) {
+                if (temp_word_2.substr(0, 2) == "0x" && flag_2 == 1) {
+                    flag_2 = 1 - flag_2;
+                    pc_present = temp_word_2;
+                }
+            }
 
-            if (is_branch) {
-                label = stoi(temp_word);
+            long label;
+            long Taken;
+
+            if (is_branch && temp.find("core") != string::npos) {
+                label = stol(temp_word);
+
+                uint32_t dec_last_pc = stoll(pc, nullptr, 16);
+                uint32_t res;
+                if (is_pos) {
+                    res = dec_last_pc + label;
+                } else {
+                    res = dec_last_pc - label;
+                }
+
+                uint32_t dec_curr = stoll(pc_present, nullptr, 16);
                 total++;
-                if (label == 4 && is_pos == 1) {
+                if (dec_curr != res) {
                     Taken = 0;
                     always_not_taken++;
                 } else {
@@ -114,20 +139,26 @@ int main() {
                     always_taken++;
                 }
 
-                if (BTB.find(pc) != BTB.end()) {
+                if (BTBonebit.find(pc) != BTBonebit.end()) {
                     single_bit(pc, temp_word, Taken);
                     double_bit(pc, temp_word, Taken);
                 } else {
                     history.push_back(Taken);
-                    BTB[pc] = {label, history};
+                    BTBonebit[pc] = {label, history};
+                    BTBtwobit[pc] = {label, history};
+                    BTBonebit[pc].second.push_back(1);
+                    BTBtwobit[pc].second.push_back(0);
+                    BTBtwobit[pc].second.push_back(0);
                 }
-
-                BTB[pc].second.push_back(Taken);
             }
-
-            flag = 1;
-            is_branch = 0;
         }
+
+        flag = 1;
+        flag_2 = 1;
+        is_branch = 0;
+        is_pos = 0;
+
+        last_instruction = temp;
     }
 
     float accuracy_always_taken = always_taken * 100.0 / total;
@@ -135,10 +166,10 @@ int main() {
     float accuracy1 = hit_1 * 100.0 / (hit_1 + miss_1);
     float accuracy2 = hit_2 * 100.0 / (hit_2 + miss_2);
 
-    cout << "Accuracy (Always Taken): " << accuracy_always_taken << " %" << endl;
-    cout << "Accuracy (Always Not Taken): " << accuracy_always_not_taken << " %" << endl;
-    cout << "Accuracy (Single Bit Predictor): " << accuracy1 << " %" << endl;
-    cout << "Accuracy (Two Bit Predictor): " << accuracy2 << " %" << endl;
+    cout << "Accuracy(Always Taken): " << accuracy_always_taken << " %" << endl;
+    cout << "Accuracy(Always Not Taken): " << accuracy_always_not_taken << " %" << endl;
+    cout << "Accuracy(Single bit): " << accuracy1 << " %" << endl;
+    cout << "Accuracy(Two bit): " << accuracy2 << " %" << endl;
 
     return 0;
 }
